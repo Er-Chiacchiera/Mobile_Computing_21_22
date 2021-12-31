7419b3f1bf1e77c62e0b4fe1653c075109c1faa1
Giordano <gio.andreola@stud.uniroma3.it>
2021-12-31 21:56:34 +0100

    aggiungendo i collider

--- Mobile/Assets/Script/RobotMovement.cs
+++ Mobile/Assets/Script/RobotMovement.cs
@@ -9,21 +9,29 @@ public class RobotMovement : MonoBehaviour
     private float verticalMove = 0f;
     private float speed = 1f;
     private Vector3 movement;
+    private RaycastHit2D hit;
+    private BoxCollider2D boxCollider;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0 )
+
+        //valori spostamento x e x dal joystick
         horizontalMove = joystick.Horizontal * speed;
         verticalMove = joystick.Vertical * speed;
-
         movement = new Vector3(horizontalMove, verticalMove, 0);
+
+        //spostamento
         transform.Translate(movement * Time.deltaTime);
+
+
     }
 }
