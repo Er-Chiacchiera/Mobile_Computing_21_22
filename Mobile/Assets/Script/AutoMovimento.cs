c0bcff16954efbb317e5b05e14e6604eb022531a
Giordano <gio.andreola@stud.uniroma3.it>
2022-01-04 19:14:24 +0100

    Aggiunti collider con rigidbody2d

--- Mobile/Assets/Script/AutoMovimento.cs
+++ Mobile/Assets/Script/AutoMovimento.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class AutoMovimento : MonoBehaviour
 {
-    private RaycastHit2D hit;
+    //private RaycastHit2D hit;
     private BoxCollider2D boxCollider;
-    private Vector2 position;
+    private bool colliso = false;
     // Start is called before the first frame update
     private void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        //boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        colliso = true;
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        colliso = false;
+    }
     // Update is called once per frame
     private void FixedUpdate()
     {
-        //controllo se ho l'asse y bloccata
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, 0.04f), Mathf.Abs(0.04f * Time.deltaTime), LayerMask.GetMask("Enemy", "Robot"));
-        if (hit.collider == null)
-        {
+        if (!colliso)
             //allora mi posso muovere
-            transform.position = new Vector2(transform.position.x, transform.position.y - 0.04f);
-        }
+            transform.position = new Vector2(transform.position.x, transform.position.y - (0.4f * Time.deltaTime));
     }
 }
