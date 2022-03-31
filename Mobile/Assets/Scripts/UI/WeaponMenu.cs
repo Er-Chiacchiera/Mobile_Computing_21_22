86252e66f39b42164f5562640f4bdece05c5551a
simone_albero <simone.albero29@gmail.com>
2022-03-31 21:58:06 +0200

    Introdotte Animazioni Weapon Switch

--- Mobile/Assets/Scripts/UI/WeaponMenu.cs
+++ Mobile/Assets/Scripts/UI/WeaponMenu.cs
@@ -8,15 +8,21 @@ public class WeaponMenu : MonoBehaviour
     public void Weapon1()
     {
         FindObjectOfType<Shooting>().SetIdWeapon(1);
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("disable");
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("w1");
     }
 
     public void Weapon2()
     {
         FindObjectOfType<Shooting>().SetIdWeapon(2);
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("disable");
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("w2");
     }
 
     public void Weapon3()
     {
         FindObjectOfType<Shooting>().SetIdWeapon(3);
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("disable");
+        GameObject.Find("Robot").GetComponent<Animator>().SetTrigger("w3");
     }
 }
